33b018f05ec5089de1ea33a5dfe60697585a1e81
mrsargent <sargent11235@gmail.com>
2024-07-31 09:34:48 -0500

    typescript homework 1

---
+++ src/contract-schema_ryan.ts
@@ -0,0 +1,77 @@
+import { Data, fromText, toHex, Address } from "@lucid-evolution/lucid";
+
+//redeemer type
+
+//****** easy way - less readable IMO ****************/
+// const MarketRedeemerSchema = Data.Object({
+//   BuyOrWithdraw: Data.Integer()
+// });
+
+const MarketRedeemerEnum = Data.Enum([
+  Data.Literal("Buy"),
+  Data.Literal("Withdraw")
+]);
+
+const MarketRedeemerSchema = Data.Object({
+  BuyOrWithdraw: MarketRedeemerEnum
+});
+
+type RedeemerOject = Data.Static<typeof MarketRedeemerSchema>;
+const RedeemerOject = MarketRedeemerSchema as unknown as RedeemerOject;
+
+const myRedeemer: RedeemerOject = {
+  BuyOrWithdraw: "Buy"
+};
+
+console.log(Data.to(myRedeemer,RedeemerOject));
+
+//throws error
+//console.log(Data.from("d87981d87980",RedeemerOject));
+
+
+
+// ********************************************************
+// *****  SimpleSale datum type from NFT market place *****
+// ********************************************************
+const CredentialSchema = Data.Enum([
+  Data.Object({ VKeyCredential: Data.Object({bytes: Data.Bytes(),}),}),
+  Data.Object({ StakeCredential: Data.Object({bytes: Data.Bytes(),}),})
+]);
+
+const AddressSchema = Data.Object({
+  paymentCredential: CredentialSchema,
+  stakeCredential: Data.Nullable(CredentialSchema)
+});
+
+type AddressObject = Data.Static<typeof AddressSchema>;
+const AddressObject = CredentialSchema as unknown as AddressObject;
+
+
+const SimpleSaleSchema = Data.Object({
+    sellerAddress: AddressSchema,
+    priceOfAsset: Data.Integer()
+});
+
+type SimpleSaleDatum = Data.Static<typeof SimpleSaleSchema>;
+const SimpleSaleDatum = SimpleSaleSchema as  unknown as SimpleSaleDatum;
+
+const myuSimpleSaleDatum: SimpleSaleDatum = {
+  sellerAddress:{
+    paymentCredential: {
+      VKeyCredential: {bytes: "1235"}
+    },
+    stakeCredential: null
+  },
+  priceOfAsset: 100n
+};
+
+
+const mySimpleSaleObject = Data.to(myuSimpleSaleDatum,SimpleSaleDatum);
+console.log(mySimpleSaleObject);
+
+// !!!!!!!!!!!!!!111 throws and error!
+// console.log(Data.from("d8798343313131d879811915b30a",SimpleSaleDatum));
+
+
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { generateSeedPhrase, Koios, Lucid } from "@lucid-evolution/lucid";
-import * as CML from "@anastasia-labs/cardano-multiplatform-lib-nodejs"
+//import * as CML from "@anastasia-labs/cardano-multiplatform-lib-nodejs"
 
 console.log("hello");
 
@@ -191,94 +191,9 @@ const tx = await user1
     "addr_test1qzqaf0vrfgp6rjexculvz95uh5wfwf80scuzk0pkhk0uwhtyf3p6v74w39symppcqpsnfl8g883x4gh0mmlg5lua7e0qw5w84m",
     { lovelace: 5_000_000n }
   )
-  .complete()
+  .complete();
 
 const signed = await tx.sign.withWallet().complete();
 
-// const txhash = await signed.submit();
-// console.log(txhash);
-console.log("hash", signed.toHash())
-console.log("cbor", signed.toCBOR())
-console.log("txbody()", CML.Transaction.from_cbor_hex(signed.toCBOR()).body().to_json()) 
-console.log("witness()", CML.Transaction.from_cbor_hex(signed.toCBOR()).witness_set().to_json())
-
-// transaction =
-//   [ transaction_body
-//   , transaction_witness_set
-//   , bool
-//   , auxiliary_data / null
-//   ] 
-
-// transaction_input = [ transaction_id : $hash32
-//   , index : uint .size 2
-//   ]
-// transaction_body =
-//   { 0 : set<transaction_input>             ; inputs
-//   , 1 : [* transaction_output]
-//   , 2 : coin                               ; fee
-//   , ? 3 : slot_no                          ; time to live
-//   , ? 4 : certificates
-//   , ? 5 : withdrawals
-//   , ? 7 : auxiliary_data_hash
-//   , ? 8 : slot_no                          ; validity interval start
-//   , ? 9 : mint
-//   , ? 11 : script_data_hash
-//   , ? 13 : nonempty_set<transaction_input> ; collateral inputs
-//   , ? 14 : required_signers
-//   , ? 15 : network_id
-//   , ? 16 : transaction_output              ; collateral return
-//   , ? 17 : coin                            ; total collateral
-//   , ? 18 : nonempty_set<transaction_input> ; reference inputs
-//   , ? 19 : voting_procedures               ; New; Voting procedures
-//   , ? 20 : proposal_procedures             ; New; Proposal procedures
-//   , ? 21 : coin                            ; New; current treasury value
-//   , ? 22 : positive_coin                   ; New; donation
-//   }
-
-//   transaction_witness_set =
-//   { ? 0: nonempty_set<vkeywitness> // wallet
-//   , ? 1: nonempty_set<native_script>
-//   , ? 2: nonempty_set<bootstrap_witness> // byron
-//   , ? 3: nonempty_set<plutus_v1_script>
-//   , ? 4: nonempty_set<plutus_data> // datum_hash v1
-//   , ? 5: redeemers // list of redeemers
-//   , ? 6: nonempty_set<plutus_v2_script>
-//   , ? 7: nonempty_set<plutus_v3_script>
-//   }
-
-//   vkeywitness = [ $vkey, $signature ]
-
-// [
-//   {
-//       0: [
-//           [
-//               h'805a8eea5c8e37875d05c82e1f360fa47572190486effc149a56d383ab828648',
-//               1,
-//           ],
-//       ],
-//       1: [
-//           [
-//               h'0081d4bd834a03a1cb26c73ec1169cbd1c9724ef86382b3c36bd9fc75d644c43a67aae89604d8438006134fce839e26aa2efdefe8a7f9df65e',
-//               5000000_2,
-//           ],
-//           [
-//               h'00ac28744693abc3bda6286a736b5e646f45049367503c8111c6bd324b937778b28a571bfdcb67099fd9e62edf75e019d7d2c7cde5786d8ef3',
-//               9663894_2,
-//           ],
-//       ],
-//       2: 168053_2,
-//   },
-//   {
-//       0: [
-//           [
-//               h'afe2655f36fe02e4d930c0046ac22b3d7191a219729f4505d9a9f7ada3da9864',
-//               h'4e4615df3c0c60f2ee5b9e537b1ccd2303f5f558d3d6eb1fecc1d2af8d5d881899d41d8af9b2f2769d46c54e37da87db3ae993276d00a69b0866c8516dd34302',
-//           ],
-//       ],
-//   },
-//   true,
-//   null,
-// ]
-
-
-
+const txhash = await signed.submit();
+console.log(txhash);
