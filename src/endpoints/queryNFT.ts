72fc2136b726662df060a4af7ffbf9ad4a70996e
mrsargent <sargent11235@gmail.com>
2024-08-11 14:13:16 -0500

    nft marketplace update

---
+++ src/endpoints/queryNFT.ts
@@ -0,0 +1,34 @@
+import { Data, LucidEvolution, UTxO, Validator, validatorToAddress } from "@lucid-evolution/lucid";
+import { fromAddress, SimpleSaleDatum } from "../contract-schema.js";
+
+// get all utxos
+// filter right datum
+// return [reaable_utxos]  return not cbor but price of asset and seller address 
+
+//how does the buy know what the seller address is in this???
+
+export const queryNFT = async (lucid: LucidEvolution, contractCbor: string, purchasePrice: bigint): Promise<UTxO[]> => {
+
+  const contract: Validator = {
+    type: "PlutusV2",
+    script: contractCbor,
+  };
+  const contractAddr = validatorToAddress("Preprod", contract);
+
+
+  const allContractUtxos = await lucid.utxosAt(contractAddr);
+  const allUserContractUtxos = allContractUtxos.filter(async (value) => {
+    if (value.datum) {
+      const datum = Data.from(value.datum, SimpleSaleDatum);
+      const price_equal = datum.priceOfAsset === purchasePrice;
+      const same_addr = datum.sellerAddress === fromAddress(await lucid.wallet().address());
+      price_equal && same_addr;
+      //return allContractUtxos;
+     }    
+   
+  }
+  );
+
+  return allUserContractUtxos;
+
+}
